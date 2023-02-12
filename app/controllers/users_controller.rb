0ad538d43743b5bcd49aade175ba18bf8fe00681
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-02-12 15:21:58 +0800

    Add edit form and ability to display form errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
-      flash[:success] = 'Congratulations! Your account has been created!'
     else
-      flash.now[:error] = "That's a bummer! Your registration has error(s)."
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
