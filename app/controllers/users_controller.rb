8bb7c561a52b0a1d623c74550baf06d6c36ccb73
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-02-12 12:49:17 +0800

    Add sign up form to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    #@user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+      flash[:success] = 'Congratulations! Your account has been created!'
+    else
+      flash.now[:error] = "That's a bummer! Your registration has error(s)."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
